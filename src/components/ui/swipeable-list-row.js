ef7215079e8acc86d11d51aef238aa16e96d925b
Anifowoshe Gbenga <daveanifowoshe@ymail.com>
2020-01-24 16:15:26 +0100

    Restructure the swipeable-list-row component

--- src/components/ui/swipeable-list-row.js
+++ src/components/ui/swipeable-list-row.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 import { Animated } from 'react-native'
 import styled from 'styled-components/native'
 import Swipeable from 'react-native-gesture-handler/Swipeable';
-import { ListRow } from './list'
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import { ListRow } from './list'
 
 const AnimatedView = styled(Animated.View)`
     width: 64px;
